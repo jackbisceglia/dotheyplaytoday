08226bc4d3e717a2af9c4ac9d119376092cae1dd
jackbisceglia <jackbisceglia2000@gmail.com>
2026-02-26 01:38:14 -0500

    fix(core): make dotenv loading optional in jobs config

--- packages/core/src/lib/effect/config.ts
+++ packages/core/src/lib/effect/config.ts
@@ -1,13 +1,5 @@
 import { PlatformConfigProvider } from "@effect/platform";
-import { Effect, Layer } from "effect";
 
 export function createConfigProviderFromDotEnv(path: string) {
-  const Provider = PlatformConfigProvider.fromDotEnv(path);
-
-  const ProviderLayer = Provider.pipe(
-    Effect.map(Layer.setConfigProvider),
-    Layer.unwrapEffect,
-  );
-
-  return ProviderLayer;
+  return PlatformConfigProvider.layerDotEnvAdd(path);
 }
