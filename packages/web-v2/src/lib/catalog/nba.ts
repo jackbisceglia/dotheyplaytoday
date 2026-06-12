ba1a72c16df3d9bbf400badc261d964ef29ec304
jackbisceglia <jackbisceglia2000@gmail.com>
2026-06-12 02:04:00 -0400

    refactor(web-v2): simplify nba team builder

--- packages/web-v2/src/lib/catalog/nba.ts
+++ packages/web-v2/src/lib/catalog/nba.ts
@@ -49,7 +49,7 @@ type NbaTeamBuilder = (context: NbaTeamBuilderContext) => Subject["details"];
 const makeNbaSubjectId = (index: number): Subject["id"] =>
   `00000000-0000-4000-8000-${String(index + 101).padStart(12, "0")}`;
 
-const makeNbaTeam = (index: number, fn: NbaTeamBuilder): Subject => {
+const makeNbaTeam = (fn: NbaTeamBuilder, index: number): Subject => {
   const details = fn({ make: makeSportTeamSubject });
 
   return {
@@ -59,252 +59,338 @@ const makeNbaTeam = (index: number, fn: NbaTeamBuilder): Subject => {
   };
 };
 
-const NBA_TEAM_DETAILS = [
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Atlanta",
-      name: "Hawks",
-      abbreviation: "ATL",
-      slug: "atlanta-hawks",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Boston",
-      name: "Celtics",
-      abbreviation: "BOS",
-      slug: "boston-celtics",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Brooklyn",
-      name: "Nets",
-      abbreviation: "BKN",
-      slug: "brooklyn-nets",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Charlotte",
-      name: "Hornets",
-      abbreviation: "CHA",
-      slug: "charlotte-hornets",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Chicago",
-      name: "Bulls",
-      abbreviation: "CHI",
-      slug: "chicago-bulls",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Cleveland",
-      name: "Cavaliers",
-      abbreviation: "CLE",
-      slug: "cleveland-cavaliers",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Dallas",
-      name: "Mavericks",
-      abbreviation: "DAL",
-      slug: "dallas-mavericks",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Denver",
-      name: "Nuggets",
-      abbreviation: "DEN",
-      slug: "denver-nuggets",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Detroit",
-      name: "Pistons",
-      abbreviation: "DET",
-      slug: "detroit-pistons",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Golden State",
-      name: "Warriors",
-      abbreviation: "GSW",
-      slug: "golden-state-warriors",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Houston",
-      name: "Rockets",
-      abbreviation: "HOU",
-      slug: "houston-rockets",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Indiana",
-      name: "Pacers",
-      abbreviation: "IND",
-      slug: "indiana-pacers",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "LA",
-      name: "Clippers",
-      abbreviation: "LAC",
-      slug: "la-clippers",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Los Angeles",
-      name: "Lakers",
-      abbreviation: "LAL",
-      slug: "los-angeles-lakers",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Memphis",
-      name: "Grizzlies",
-      abbreviation: "MEM",
-      slug: "memphis-grizzlies",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Miami",
-      name: "Heat",
-      abbreviation: "MIA",
-      slug: "miami-heat",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Milwaukee",
-      name: "Bucks",
-      abbreviation: "MIL",
-      slug: "milwaukee-bucks",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Minnesota",
-      name: "Timberwolves",
-      abbreviation: "MIN",
-      slug: "minnesota-timberwolves",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "New Orleans",
-      name: "Pelicans",
-      abbreviation: "NOP",
-      slug: "new-orleans-pelicans",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "New York",
-      name: "Knicks",
-      abbreviation: "NYK",
-      slug: "new-york-knicks",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Oklahoma City",
-      name: "Thunder",
-      abbreviation: "OKC",
-      slug: "oklahoma-city-thunder",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Orlando",
-      name: "Magic",
-      abbreviation: "ORL",
-      slug: "orlando-magic",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Philadelphia",
-      name: "76ers",
-      abbreviation: "PHI",
-      slug: "philadelphia-76ers",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Phoenix",
-      name: "Suns",
-      abbreviation: "PHX",
-      slug: "phoenix-suns",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Portland",
-      name: "Trail Blazers",
-      abbreviation: "POR",
-      slug: "portland-trail-blazers",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Sacramento",
-      name: "Kings",
-      abbreviation: "SAC",
-      slug: "sacramento-kings",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "San Antonio",
-      name: "Spurs",
-      abbreviation: "SAS",
-      slug: "san-antonio-spurs",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Toronto",
-      name: "Raptors",
-      abbreviation: "TOR",
-      slug: "toronto-raptors",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Utah",
-      name: "Jazz",
-      abbreviation: "UTA",
-      slug: "utah-jazz",
-    }),
-  (ctx) =>
-    ctx.make({
-      leagueId: "nba",
-      location: "Washington",
-      name: "Wizards",
-      abbreviation: "WAS",
-      slug: "washington-wizards",
-    }),
-] satisfies readonly NbaTeamBuilder[];
-
-export const NBA_TEAMS = NBA_TEAM_DETAILS.map((fn, index) =>
-  makeNbaTeam(index, fn),
-) satisfies readonly Subject[];
+export const NBA_TEAMS = [
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Atlanta",
+        name: "Hawks",
+        abbreviation: "ATL",
+        slug: "atlanta-hawks",
+      }),
+    0,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Boston",
+        name: "Celtics",
+        abbreviation: "BOS",
+        slug: "boston-celtics",
+      }),
+    1,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Brooklyn",
+        name: "Nets",
+        abbreviation: "BKN",
+        slug: "brooklyn-nets",
+      }),
+    2,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Charlotte",
+        name: "Hornets",
+        abbreviation: "CHA",
+        slug: "charlotte-hornets",
+      }),
+    3,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Chicago",
+        name: "Bulls",
+        abbreviation: "CHI",
+        slug: "chicago-bulls",
+      }),
+    4,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Cleveland",
+        name: "Cavaliers",
+        abbreviation: "CLE",
+        slug: "cleveland-cavaliers",
+      }),
+    5,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Dallas",
+        name: "Mavericks",
+        abbreviation: "DAL",
+        slug: "dallas-mavericks",
+      }),
+    6,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Denver",
+        name: "Nuggets",
+        abbreviation: "DEN",
+        slug: "denver-nuggets",
+      }),
+    7,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Detroit",
+        name: "Pistons",
+        abbreviation: "DET",
+        slug: "detroit-pistons",
+      }),
+    8,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Golden State",
+        name: "Warriors",
+        abbreviation: "GSW",
+        slug: "golden-state-warriors",
+      }),
+    9,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Houston",
+        name: "Rockets",
+        abbreviation: "HOU",
+        slug: "houston-rockets",
+      }),
+    10,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Indiana",
+        name: "Pacers",
+        abbreviation: "IND",
+        slug: "indiana-pacers",
+      }),
+    11,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "LA",
+        name: "Clippers",
+        abbreviation: "LAC",
+        slug: "la-clippers",
+      }),
+    12,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Los Angeles",
+        name: "Lakers",
+        abbreviation: "LAL",
+        slug: "los-angeles-lakers",
+      }),
+    13,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Memphis",
+        name: "Grizzlies",
+        abbreviation: "MEM",
+        slug: "memphis-grizzlies",
+      }),
+    14,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Miami",
+        name: "Heat",
+        abbreviation: "MIA",
+        slug: "miami-heat",
+      }),
+    15,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Milwaukee",
+        name: "Bucks",
+        abbreviation: "MIL",
+        slug: "milwaukee-bucks",
+      }),
+    16,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Minnesota",
+        name: "Timberwolves",
+        abbreviation: "MIN",
+        slug: "minnesota-timberwolves",
+      }),
+    17,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "New Orleans",
+        name: "Pelicans",
+        abbreviation: "NOP",
+        slug: "new-orleans-pelicans",
+      }),
+    18,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "New York",
+        name: "Knicks",
+        abbreviation: "NYK",
+        slug: "new-york-knicks",
+      }),
+    19,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Oklahoma City",
+        name: "Thunder",
+        abbreviation: "OKC",
+        slug: "oklahoma-city-thunder",
+      }),
+    20,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Orlando",
+        name: "Magic",
+        abbreviation: "ORL",
+        slug: "orlando-magic",
+      }),
+    21,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Philadelphia",
+        name: "76ers",
+        abbreviation: "PHI",
+        slug: "philadelphia-76ers",
+      }),
+    22,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Phoenix",
+        name: "Suns",
+        abbreviation: "PHX",
+        slug: "phoenix-suns",
+      }),
+    23,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Portland",
+        name: "Trail Blazers",
+        abbreviation: "POR",
+        slug: "portland-trail-blazers",
+      }),
+    24,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Sacramento",
+        name: "Kings",
+        abbreviation: "SAC",
+        slug: "sacramento-kings",
+      }),
+    25,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "San Antonio",
+        name: "Spurs",
+        abbreviation: "SAS",
+        slug: "san-antonio-spurs",
+      }),
+    26,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Toronto",
+        name: "Raptors",
+        abbreviation: "TOR",
+        slug: "toronto-raptors",
+      }),
+    27,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Utah",
+        name: "Jazz",
+        abbreviation: "UTA",
+        slug: "utah-jazz",
+      }),
+    28,
+  ),
+  makeNbaTeam(
+    (ctx) =>
+      ctx.make({
+        leagueId: "nba",
+        location: "Washington",
+        name: "Wizards",
+        abbreviation: "WAS",
+        slug: "washington-wizards",
+      }),
+    29,
+  ),
+] satisfies readonly Subject[];
 
 export const getLogo = (abbr: string) => NBA_LOGOS[abbr] ?? FALLBACK_LOGO;
 
