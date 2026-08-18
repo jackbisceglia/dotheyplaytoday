ac1cd56e2c2b24b0b3f13bf5cdc78fd4c22b6d79
Jack Bisceglia <57922742+jackbisceglia@users.noreply.github.com>
2026-08-18 03:57:07 -0400

    feat(web): add Dot Peeking brand mark (#92)

--- packages/web/src/Document.tsx
+++ packages/web/src/Document.tsx
@@ -7,6 +7,7 @@ export default function Document(props: ParentProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
         <HydrationScript />
       </head>
       <body>{props.children}</body>
