b28ea38e8a37caa7d94b5f636879fd00c10186d1
jackbisceglia <jackbisceglia2000@gmail.com>
2026-08-26 23:31:35 -0400

    refactor(feedback): distinguish ops sender

--- packages/jobs/src/ops/worker.ts
+++ packages/jobs/src/ops/worker.ts
@@ -13,7 +13,7 @@ const FeedbackScheduleTwiceDailyUtc = "0 0,12 * * *";
 
 const EmailLayerOps = makeEmailLayerResend({
   from: {
-    name: "dotheyplaytoday ops",
+    name: "Feedback from dotheyplaytoday",
     email: EmailAddress.make("ops@dotheyplay.today"),
   },
 });
